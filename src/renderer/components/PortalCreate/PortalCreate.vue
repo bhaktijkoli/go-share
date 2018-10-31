f41a80e8101259f3252cf925d8a7fa584b513dc5
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-10-31 09:44:31 +0530

    Added checkout api

--- src/renderer/components/PortalCreate/PortalCreate.vue
+++ src/renderer/components/PortalCreate/PortalCreate.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="wrapper">
-
+    <Loading/>
   </div>
 </template>
 
 <script>
 import {ipcRenderer} from 'electron'
+
+import Loading from './../Loading'
 export default {
+  components: {
+    Loading
+  },
   mounted() {
     ipcRenderer.send('portal-create');
     ipcRenderer.on('portal-created', ()=> {
